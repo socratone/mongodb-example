ff0e8922dac165397deca0e10319275044e89bb2
John Kim <john@gymtinc.com>
2022-02-02 13:00:38 +0900

    fix: validation 추가

--- init.ts
+++ init.ts
@@ -11,8 +11,24 @@ const init = async () => {
     const database = client.db('mongodb-example');
     await database.dropDatabase(); // database 삭제
 
+    await database.createCollection('users', {
+      validator: {
+        $jsonSchema: {
+          bsonType: 'object',
+          required: ['name'],
+          properties: {
+            name: {
+              bsonType: 'string',
+              description: 'must be a string and is required',
+            },
+          },
+        },
+      },
+    });
+
     const users = database.collection('users');
     // document를 생성할 때 부모인 db와 collection이 없으면 자동으로 생성한다.
+    // 그러나 위에서 validator 설정을 위해 createCollection을 실행했다.
     const result = await users.insertMany([
       { name: 'John' },
       { name: 'Peter' },
